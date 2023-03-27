66d67a8ffecc2886790c613bf2fcc95b450c3b24
Wanyom <129059176+Wanyom@users.noreply.github.com>
2023-03-27 21:18:29 +0300

    Add files via upload
    Версия 1.1

--- Alawar WRP Crack.py
+++ Alawar WRP Crack.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-print("! Чтобы кряк работал коррекно введите путь без русских букв !\n")
+print("[Alawar WRP Crack] Чтобы программа работала корректно\nпереустановите игру с путём который не содержит кириллицу и пробелов\n")
 
 import os, sys, shutil
 
@@ -12,17 +12,16 @@ path_to_program = os.getcwd()
 path1 = f"{path_to_program}"
 username = os.getlogin()
 
-print("Alawar WRP Crack, версии 1.0")
+print("Alawar WRP Crack, версии 1.1")
 print("(c) Wanyom | Все права защищены")
-print(f"Добро пожаловать, {username}!")
 
 path_to_game = input('\n[Alawar WRP Crack] Введи полный путь до игры (Например: D:\\Games\\SuperCow): ')
 if not path_to_game:
 	print("[Alawar WRP Crack] Ты не указал путь до игры!")
-	input("Нажми Enter, чтобы выйти!")
+	input("[Alawar WRP Crack] Нажми Enter, чтобы выйти!")
 	sys.exit()
 
-question = input("\n[Alawar WRP Crack] Вы действительно хотите модифицировать файлы игры? (да / нет): ")
+question = input("[Alawar WRP Crack] Вы действительно хотите модифицировать файлы игры? (да / нет): ")
 if question.lower() == 'да':
 	pass
 else:
@@ -32,18 +31,17 @@ try:
 	os.remove(f'{path_to_game}\\wrapper.dll')
 except:
 	print("[Alawar WRP Crack] Не удалось удалить 'wrapper.dll', возможно ты указал неверный путь до игры!")
-	input("Нажми Enter, чтобы выйти!")
+	input("[Alawar WRP Crack] Нажми Enter, чтобы выйти!")
 	sys.exit()
 else:
-	print("[Alawar WRP Crack] 'wrapper.dll' удалён, продолжаю взламывать игру...")
+	print("[Alawar WRP Crack] 'wrapper.dll' удалён, копирую модифицированный файл...")
 	try:
 		shutil.copy(f"{path1}\\wrapper.dll", f"{path_to_game}\\")
 	except:
-		print(Error)
 		print("[Alawar WRP Crack] Не удалось завершить патч игры из-за ошибки...")
-		input("Нажми Enter, чтобы выйти!")
+		input("[Alawar WRP Crack] Нажми Enter, чтобы выйти!")
 		sys.exit()
 	else:
 		print("[Alawar WRP Crack] Патч игры завершён успешно!")
-		input("Нажми Enter, чтобы выйти!")
+		input("[Alawar WRP Crack] Нажми Enter, чтобы выйти!")
 		sys.exit()
